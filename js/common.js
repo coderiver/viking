66b78c474c05ef021c724be9e8c8ead923202d18
Valera19900 <kaktuz34@gmail.com>
2014-03-17 11:13:28 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -38,4 +38,52 @@ $(document).ready(function() {
 			event.stopPropagation();
 		});
 
+
+//slider collection
+function sl_collection () {
+	var el = $('.js-sl-collection');
+	var prev = el.find('.collection__prev');
+	prev.addClass('is-disabled');
+	var next = el.find('.collection__next');
+	var el_in = el.find('.collection__in');
+	var item = el.find('.collection__block');
+	item.first().addClass('is-active');
+	prev.on('click', function(){
+		next.removeClass('is-disabled');
+		var act = el.find('.collection__block.is-active');
+		var last = act.prev();
+		if (last.length) {
+			act.removeClass('is-active');
+			act.prev().addClass('is-active');
+			var item_width = act.prev().width();
+			el_in.animate({scrollLeft: '-='+item_width+'px'}, 200, 'linear');
+		};
+		if (!last.prev().length) {
+			$(this).addClass('is-disabled');
+		};
+	});
+	next.on('click', function(){
+		prev.removeClass('is-disabled');
+		var act = el.find('.collection__block.is-active');
+		var last = act.next().next().next();
+		if (last.length) {
+			act.removeClass('is-active');
+			act.next().addClass('is-active');
+			var item_width = act.next().width();
+			el_in.animate({scrollLeft: '+='+item_width+'px'}, 200, 'linear');
+		};
+		if (!last.next().length) {
+			$(this).addClass('is-disabled');
+		};
+	});
+	$(window).resize(function(){
+		prev.addClass('is-disabled');
+		next.removeClass('is-disabled');
+		item.first().addClass('is-active');
+		el_in.scrollLeft(0);
+	});
+}
+sl_collection();
+
+
 });
